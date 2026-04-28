b294ea558baf3afa48179d9c369ee73cefe03ea2
kinka90 <kinkinka90@gmail.com>
2026-04-28 11:25:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6273,10 +6273,8 @@ function translateFromVoice(text){
           4. Pilih arti kata yang paling tepat berdasarkan konteks
           5. Jika ada kata ambigu (contoh: "gulaha"), pilih arti paling sesuai
           6. Jangan terjemahkan ulang dari nol, gunakan hasil kamus sebagai dasar
-          7. Jika ada kata belum tepat, boleh disesuaikan secara kontekstual
-          
-          paling penting jangan mengilangkan inputan kalimat, tapi hanya menyusunnya menjadi kalimat yang sempurna
-
+          7. Jangan mengilangkan inputan kalimat, tapi hanya menyusunnya menjadi kalimat yang sempurna
+ 
           PENTING (IMBUHAN BAHASA DAERAH):
           Jika ada kata yang mengandung imbuhan seperti:
           "o", "i", "ai", "da", "dha", "k", "n", "se", "ma", "ka"
@@ -6291,17 +6289,7 @@ function translateFromVoice(text){
           o fala =  rumah
           i fala = = rumah
           fala ni =  rumah
-          
-          contoh:
-          input: "jalan baru itu banyak debu
-          output: "ngoko sungi ge dofu fika" 
 
-          input: ""
-
-          Output:
-          - hanya 1 kalimat terbaik
-          - alami
-          - sesuai konteks
           `
         },
         {
